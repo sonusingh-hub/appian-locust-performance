a301f5217c4d7a388a9daa86de22dfa6d3fc55a7
Edward Bross <edward.bross@appian.com>
2023-04-27 15:18:27 -0500

    Update admin test

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,49 +1,33 @@
 import unittest
 
-import locust
-from appian_locust import AppianTaskSet
-from appian_locust.helper import ENV
-from appian_locust.uiform import SailUiForm
-from locust import Locust, TaskSet
-
-from .mock_client import CustomLocust
-from .mock_reader import read_mock_file
+from tests.mock_client import CustomLocust
+from tests.mock_reader import read_mock_file
+from appian_locust._admin import ADMIN_URI_PATH, _Admin
+from appian_locust._interactor import _Interactor
+from locust import Locust
 
 
 class TestAdmin(unittest.TestCase):
+    admin = read_mock_file("admin_console_landing_page.json")
+
     def setUp(self) -> None:
         self.custom_locust = CustomLocust(Locust())
-        parent_task_set = TaskSet(self.custom_locust)
-        setattr(parent_task_set, "host", "")
-        setattr(parent_task_set, "auth", ["admin_user", ""])
-        self.task_set = AppianTaskSet(parent_task_set)
-        self.task_set.host = ""
+        setattr(self.custom_locust.client, "feature_flag", "")
+        setattr(self.custom_locust.client, "feature_flag_extended", "")
 
-        # test_on_start_auth_success is covered here.
-        self.custom_locust.set_response("auth?appian_environment=tempo", 200, '{}')
-        self.task_set.on_start()
+        self.interactor = _Interactor(self.custom_locust.client, "")
+        self.interactor.login(["", ""])
+        self.admin_interactor: _Admin = _Admin(self.interactor)
 
-    def test_visit(self) -> None:
-        self.custom_locust.set_response("/suite/rest/a/applications/latest/app/admin", 200, read_mock_file("admin_console_landing_page.json"))
-        ENV.stats.clear_all()
-        sail_form = self.task_set.appian.admin.visit()
-        self.assertEqual(type(sail_form), SailUiForm)
-        self.assertEqual(0, len(ENV.stats.errors))
+        self.custom_locust.set_response(ADMIN_URI_PATH, 200, self.admin)
 
-    def test_visit_error(self) -> None:
-        ENV.stats.clear_all()
-        self.custom_locust.set_response("/suite/rest/a/applications/latest/app/admin", 401, "")
-        with self.assertRaises(Exception) as context:
-            sail_form = self.task_set.appian.admin.visit()
-        # Two errors will be logged, one at the get_page request level, and one at the visit
-        self.assertEqual(2, len(ENV.stats.errors))
+    def test_fetch_admin_json(self) -> None:
+        # Given setup,
+        # When:
+        admin = self.admin_interactor.fetch_admin_json()
 
-        # Assert error structure
-        error: locust.stats.StatsError = list(ENV.stats.errors.values())[1]
-        self.assertEqual('DESC: No description', error.method)
-        self.assertEqual('LOCATION: _admin.py/visit()', error.name)
-        self.assertEqual('EXCEPTION: 401 Client Error: None for uri: /suite/rest/a/applications/latest/app/admin Username: admin_user', error.error)
-        self.assertEqual(1, error.occurrences)
+        # Then:
+        self.assertIsInstance(admin, dict)
 
 
 if __name__ == '__main__':
