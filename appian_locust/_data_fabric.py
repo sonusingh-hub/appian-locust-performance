2ad4bcc51ffd60cca7395dc81d771e301edc884f
Kara Nardi <kara.nardi@appian.com>
2024-04-25 12:32:55 -0400

    AN-267119: Add Ability to Navigate to Dashbaords
    Adds the ability to naviate to a Data Fabric Insights Dashboard

--- appian_locust/_data_fabric.py
+++ appian_locust/_data_fabric.py
@@ -3,6 +3,7 @@ from typing import Optional, Dict, Any
 from ._interactor import _Interactor
 
 DATA_FABRIC_URI_PATH = "/suite/rest/a/applications/latest/app/process-hq"
+DATA_FABRIC_DASHBOARD_URI_PATH = "/suite/rest/a/applications/latest/app/process-hq/dashboard/"
 
 
 class _DataFabric:
@@ -14,5 +15,14 @@ class _DataFabric:
         headers['X-Client-Mode'] = 'DESIGN'
         label = locust_request_label or "DataFabric.Ui"
         response = self.interactor.get_page(DATA_FABRIC_URI_PATH, headers=headers, label=label)
+        response.raise_for_status()
+        return response.json()
+
+    def fetch_data_fabric_dashboard_json(self, encoded_uri_stub: str = "new", locust_request_label: Optional[str] = None) -> Dict[str, Any]:
+        headers = self.interactor.setup_sail_headers()
+        headers['X-Client-Mode'] = 'DESIGN'
+        label = locust_request_label or "DataFabricDashboard.Ui"
+        response = self.interactor.get_page(f"{DATA_FABRIC_DASHBOARD_URI_PATH}{encoded_uri_stub}", headers=headers,
+                                            label=label)
         response.raise_for_status()
         return response.json()
