d263bc41e11d6dc51dd81b1c213baa187c9cbe57
DSmithMagMutual <dsmith2@magmutual.com>
2024-10-21 17:55:07 +0000

    Update _interactor.py with portals_mode to trigger username auth

--- appian_locust/_interactor.py
+++ appian_locust/_interactor.py
@@ -337,7 +337,7 @@ class _Interactor:
         with self.client.get(uri, **kwargs) as resp:  # type: ResponseContextManager
             if check_login and not self.portals_mode:
                 self.check_login(resp)
-            username = get_username(self.auth)
+            username = "No-auth User" if self.portals_mode else get_username(self.auth)
             test_response_for_error(
                 resp,
                 uri,
