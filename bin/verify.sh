3e449d38ec3008cb85bc0451aebde8f5e238d523
Tanvee Badheka <tanvee.badheka@appian.com>
2025-09-08 11:06:23 -0700

    Fix AppianClient Test and verify.sh to meet latest pytest requirements

--- bin/verify.sh
+++ bin/verify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-pipenv run pytest --cov-report term --cov-report html --cov=appian_locust tests/
+pipenv run pytest --cov-report=term --cov-report=html --cov=appian_locust tests/
 pipenv run python -m pycodestyle appian_locust/*.py tests/*.py
 pipenv run mypy --disallow-untyped-defs appian_locust/*.py tests/*.py --config-file mypy/mypi.ini
